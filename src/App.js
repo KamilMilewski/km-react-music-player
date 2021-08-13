060fb25b4dbd7510f08eadb4577f863103168ef8
Kamil Milewski <kamil.milewski@hotmail.com>
2021-08-13 15:40:54 +0200

    Extract song player into its own component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./styles.css";
 
+function Header() {
+  return <h1>Hello CodeSandbox</h1>;
+}
+
+function SongPlayer() {
+  return (
+    <section>
+      <Header />
+      <audio controls src="https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3"/>
+    </section>
+  )
+}
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <audio controls src="https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3"/>
+      <SongPlayer />
     </div>
   );
 }
