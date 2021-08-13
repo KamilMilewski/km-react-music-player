59bbd252e29ffdcf0bbbfebc3083a1ac5db97e71
Kamil Milewski <kamil.milewski@hotmail.com>
2021-08-13 15:27:36 +0200

    Add native audio component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Let's create a Music Player</h2>
+      <audio controls src="https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3"/>
     </div>
   );
 }
