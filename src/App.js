d754e1463aab29c535b4098a69439d74a9494ff0
Kamil Milewski <kamil.milewski@hotmail.com>
2021-08-14 11:16:57 +0200

    Add song cover

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ function Heading({title}) {
   return <h1>{title}</h1>;
 }
 
-function SongPlayer({showControls = true, audioUrl}) {
+function SongPlayer({showControls = true, song}) {
+  const { audioUrl, coverUrl } = song;
   return (
     <section>
       <Heading title="Music Player"/>
+      <img width="250px" height="250px" src={coverUrl} alt="Song cover"/>
       <audio controls={ showControls } >
 	<source src={ audioUrl } />
       </audio>
@@ -16,9 +18,16 @@ function SongPlayer({showControls = true, audioUrl}) {
 }
 
 export default function App() {
+  const currentSong = {
+    audioUrl: "https://examples.devmastery.pl/assets/audio/deadfro5h.mp3",
+    coverUrl: "https://examples.devmastery.pl/assets/audio/deadfro5h.jpg",
+    title: "Deadfro5h",
+    artist: "starfrosh"
+  }
   return (
     <div className="App">
-      <SongPlayer audioUrl={ "https://examples.devmastery.pl/assets/audio/deadfro5h.mp3" }/>
+      <SongPlayer song={ currentSong }
+      />
     </div>
   );
 }
