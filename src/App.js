c400b8369f6f59d3da9f3d0a80a3acd69163cccc
Kamil Milewski <kamil.milewski@hotmail.com>
2021-08-14 09:59:25 +0200

    Add something

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import "./styles.css";
 
-function Header() {
+function Heading() {
+  const showHello = 1 + 1 === 3 || 2 === 2
   return <h1>Hello CodeSandbox</h1>;
 }
 
 function SongPlayer() {
+  const showControls = false || true;
+  const audioUrl = "https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3"
   return (
     <section>
-      <Header />
-      <audio controls src="https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3"/>
+      <Heading />
+      <audio controls={ showControls } src={ audioUrl }/>
     </section>
   )
 }
 
+function getStatusMessage(isLoading, hasErrors) {
+  let message = null;
+  if (isLoading) {
+    message = "Loading...";
+  }
+  if (hasErrors) {
+    message = "Error occured...";
+  }
+  return message;
+}
+
 export default function App() {
+  const isLoading = false;
+  const hasErrors = false;
+  const showPlayer = !isLoading && !hasErrors;
+  const statusMessage = getStatusMessage(isLoading, hasErrors);
+
   return (
-    <div className="App">
-      <SongPlayer />
-    </div>
+    <div className="App">{showPlayer ? <SongPlayer /> : statusMessage}</div>
   );
 }
