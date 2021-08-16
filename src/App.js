1bd963b605e7c07a4286b81ef9c43c86ddc00e27
Kamil Milewski <kamil.milewski@hotmail.com>
2021-08-16 16:02:23 +0200

    Implement play & pause buttons

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import "./styles.css";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 function Heading({title}) {
   return <h1>{title}</h1>;
 }
 
-function SongPlayer({showControls = true, song}) {
+function SongPlayer({showControls = false, song}) {
+  const audioRef = useRef();
   const { audioUrl, coverUrl } = song;
   return (
     <section>
       <Heading title="Music Player"/>
       <img width="250px" height="250px" src={coverUrl} alt="Song cover"/>
-      <audio key={song.audioUrl} controls={ showControls } >
+      <audio ref={audioRef} key={song.audioUrl} controls={ showControls } >
 	<source src={ audioUrl } />
       </audio>
+      <div>
+	<button onClick={() => audioRef.current.play()}>Play</button>
+	<button onClick={() => audioRef.current.pause()}>Pause</button>
+      </div>
     </section>
   )
 }
