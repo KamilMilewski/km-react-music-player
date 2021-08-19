40084d7a59efb3ae9486ecb61f2a46162aa84375
Kamil Milewski <kamil.milewski@hotmail.com>
2021-08-19 19:18:00 +0200

    Calculate percentage position of click on the progress bar

--- src/components/SongProgress.js
+++ src/components/SongProgress.js
@@ -1,11 +1,31 @@
+import React, { useRef, useEffect } from "react";
 import "./SongProgress.css";
 
 export function SongProgress({progress}) {
+  const progressBarRef = useRef();  
   const progressFormatted = Math.floor(progress);
+
+  useEffect(() => {
+    progressBarRef.current.addEventListener("mousedown", function(e) {
+	// Get the target
+	const target = e.target;
+
+	// Get the bounding rectangle of target
+	const rect = target.getBoundingClientRect();
+
+	// Mouse position
+	const x = e.clientX - rect.left;
+
+	const width = rect.width
+	const desiredSongProgress = (x/width) * 100
+
+	console.log(desiredSongProgress)
+    });
+  },[]);
 
   return (
     <section className="SongProgress" >
-      <div id="ProgressBarContainer">
+      <div ref={ progressBarRef } id="ProgressBarContainer">
 	<div id="ProgressBar" style={{ width: `${progressFormatted}%`}}></div>
       </div>
       {progressFormatted} %
